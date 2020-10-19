e66d31ff0e1757d2369e6351598a11702ccb1925
Nehalkumar99 <nehal0000kumar1999@gmail.com>
2020-10-19 23:21:40 +0530

    creating repo on github

---
+++ Scripts/MainGamePlay.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class MainGamePlay : MonoBehaviour
+{
+    [SerializeField] Text textElement;
+    [SerializeField] States startState;
+    [SerializeField] Text button1Text;
+    [SerializeField] Text button2Text;
+    [SerializeField] Text button3Text;
+    States state;
+    
+    void Start()
+    {
+        state = startState;
+        textElement.text = state.GetStateText();
+        button1Text.text = state.GetButton1Text();
+        button2Text.text = state.GetButton2Text();
+        button3Text.text = state.GetButton3Text();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameMain();
+    }
+
+    // Used to respond to user input
+     void GameMain()
+    {
+        
+       textElement.text = state.GetStateText();
+       button1Text.text = state.GetButton1Text();
+       button2Text.text = state.GetButton2Text();
+       button3Text.text = state.GetButton3Text();
+    }
+
+   public void Button1Press()
+    {
+        States[] statesArray = state.GetOtherStates();
+        state = statesArray[0];
+    }
+
+   public void Button2Press()
+    {
+        States[] statesArray = state.GetOtherStates();
+        state = statesArray[1];
+    }
+
+    public void Button3Press()
+    {
+        States[] statesArray = state.GetOtherStates();
+        state = statesArray[2];
+    }
+
+}
---
+++ Scripts/States.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "States")]
+public class States : ScriptableObject
+{
+    [SerializeField] string gameText;
+    [SerializeField] string button1String;
+    [SerializeField] string button2String;
+    [SerializeField] string button3String;
+    [SerializeField] States[] otherStates;
+     // Start is called before the first frame update
+    public string GetStateText()
+    {
+        return gameText;
+      
+    }
+
+    public string GetButton1Text()
+    {
+        return button1String;
+    }
+   
+    public string GetButton2Text()
+    {
+        return button2String;
+    }
+    public string GetButton3Text()
+    {
+        return button3String;
+    }
+    public States[] GetOtherStates()
+    {
+        return otherStates;
+    }
+   
+}
